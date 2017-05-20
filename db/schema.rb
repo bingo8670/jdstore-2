8147367ea8b3d12c2d57b1a3ad64097201d43391
bingo8670 <307286130@qq.com>
2017-05-20 17:16:26 +0800

    12-4/6 don't work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519030040) do
+ActiveRecord::Schema.define(version: 20170519075715) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
