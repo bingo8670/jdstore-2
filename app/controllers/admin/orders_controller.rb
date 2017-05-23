fe74b5305297b27951b8296b8f16025a1e23fea4
bingo8670 <307286130@qq.com>
2017-05-23 11:16:26 +0800

    add db:seed but step4/6 don't done yet

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -23,13 +23,13 @@ class Admin::OrdersController < ApplicationController
   def shipped
     @order = Order.find(params[:id])
     @order.deliver!
-    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
